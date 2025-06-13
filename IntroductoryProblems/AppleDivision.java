5418177ce9e664a826abb7127968a9d5b8baaf41
Viba Mohan <happymorning552@gmail.com>
2025-06-13 11:39:08 -0700

    Solve a few more problems
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>

---
+++ IntroductoryProblems/AppleDivision.java
@@ -0,0 +1,5 @@
+public class AppleDivision {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ IntroductoryProblems/BitStrings.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BitStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+
+        
+        
+        // System.out.println(ans);
+    }
+}
