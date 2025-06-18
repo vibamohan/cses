4f44ab42a76eef0d19e0e3f852abbf0d93e4931a
Viba Mohan <happymorning552@gmail.com>
2025-06-17 18:03:22 -0700

    Add review list and solve more:
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>

---
+++ SortingAndSearching/MissingCoinSum.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MissingCoinSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int kNumCoins = scanner.nextInt();
+        int[] coins = new int[kNumCoins];
+
+        for (int i = 0; i < kNumCoins; i++) {
+            coins[i] = scanner.nextInt();
+        }
+
+        /** IDK  */
+        Arrays.sort(coins);
+        System.out.println(Arrays.toString(coins));
+
+        int achievableSum = 0;
+        for (int i = 0; i < kNumCoins; i++) {
+            int curSum = achievableSum + 1;
+            if (coins[i] > curSum) {
+                System.out.println(curSum + 1);
+                return;
+            }
+            achievableSum += i;
+        }
+
+        System.out.println(achievableSum + 1);
+    }
+}
---
+++ SortingAndSearching/StickLengths.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class StickLengths {
+  public static void main(String[] args) {
+    final Scanner scanner = new Scanner(System.in);
+    int kN = scanner.nextInt();
+
+    int[] sticks = new int[kN];
+    for (int i = 0; i < kN; i++) {
+      sticks[i] = scanner.nextInt();
+    }
+
+    Arrays.sort(sticks);
+    final int median = sticks[kN / 2];
+
+    long totalCost = 0;
+    for (int i = 0; i < kN; i++) {
+      totalCost += Math.abs(sticks[i] - median);
+    }
+
+    System.out.println(totalCost);
+  }
+}
