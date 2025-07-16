0322bc67926b919e3e1c34b935cfe27d423e0587
Viba Mohan <happymorning552@gmail.com>
2025-07-15 22:31:44 -0700

    fix up small issues
    Signed-off-by: Viba Mohan <happymorning552@gmail.com>

--- GraphAlgorithmns/RoundTrips.cc
+++ GraphAlgorithmns/RoundTrips.cc
@@ -41,128 +41,140 @@
 
 class UnionFind {
 public:
-    UnionFind(int n) {
-        rank.resize(n + 1, 1);
-        parent.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-        }
+  UnionFind(int n) {
+    rank.resize(n + 1, 1);
+    parent.resize(n + 1);
+    for (int i = 0; i <= n; i++) {
+      parent[i] = i;
+    }
+  }
+
+  int find(int a) {
+    if (a == parent[a])
+      return a;
+    // path compression, recursively find parent,
+    // but also update it inplace so that we just have the root in parent
+    // instead of layers of links. This is what we need and reduces time taken
+    // in future runs
+    return parent[a] = find(parent[a]);
+  }
+
+  bool union_sets(int a, int b) {
+    int find_a = find(a), find_b = find(b);
+
+    // Cycle because they are both part of the same tree
+    if (find_a == find_b)
+      return false;
+
+    // pick smaller rank for time complexity later
+    if (rank[find_a] < rank[find_b]) {
+      std::swap(find_a, find_b);
     }
 
-    int find(int a) {
-        if (a == parent[a]) return a;
-        return parent[a] = find(parent[a]);
-    }
+    // join
+    parent[find_b] = find_a;
 
-    bool union_sets(int a, int b) {
-        int find_a = find(a), find_b = find(b);
-
-        // Cycle because they are both part of the same tree
-        if (find_a == find_b) return false;
-
-        // pick smaller rank for time complexity later
-        if (rank[find_a] < rank[find_b]) { 
-            std::swap(find_a, find_b);
-        }
-
-        // join
-        parent[find_b] = find_a;
-
-        // if they are the same rank just increase depth by 1
-        // i.e attach it right before the root to only increase depth by 1
-        if (rank[find_a] == rank[find_b]) rank[find_a]++;
-        return true;
-    }
+    // if they are the same rank just increase depth by 1
+    // i.e attach it right before the root to only increase depth by 1
+    if (rank[find_a] == rank[find_b])
+      rank[find_a]++;
+    return true;
+  }
 
 private:
-    std::vector<int> parent, rank;
+  std::vector<int> parent, rank;
 };
 
-std::vector<std::vector<int>> adj;
+std::vector<std::vector<int> > adj;
 std::vector<int> path;
 std::vector<int> parent;
 std::vector<bool> visited;
 bool cycled = false;
 
 void dfs(int start, int prev) {
-    // I think we could also do this in bfs, but its more logical in dfs and i like this way more
-    if (cycled) return;
-    visited[start] = true;
-    parent[start] = prev;
-    for (int neighbour : adj[start]) {
-        if (neighbour == prev) continue;
-        // we found a cycle so we're backtraversing 
-        if (visited[neighbour]) {
-            // push neighbour as it is our src
-            path.push_back(neighbour);
-            int cur = start;
+  // I think we could also do this in bfs, but its more logical in dfs and i
+  // like this way more
+  if (cycled)
+    return;
+  visited[start] = true;
+  parent[start] = prev;
+  for (int neighbour : adj[start]) {
+    if (neighbour == prev)
+      continue;
+    // we found a cycle so we're backtraversing
+    if (visited[neighbour]) {
+      // push neighbour as it is our src
+      path.push_back(neighbour);
+      int cur = start;
 
-            // keep traversing till we meet neighbour again (avoid infinite cycle traversing)
-            while (cur != neighbour) {
-                path.push_back(cur);
-                cur = parent[cur];
-            }
+      // keep traversing till we meet neighbour again (avoid infinite cycle
+      // traversing)
+      while (cur != neighbour) {
+        path.push_back(cur);
+        cur = parent[cur];
+      }
 
-            // add neighbour back since it is a cycle (round trip)
-            // we also reverse since we have going from last seen node -> beginning
-            // which is reverse of the order the question wants 
-            path.push_back(neighbour);
-            std::reverse(path.begin(), path.end());
-            cycled = true;
-            return;
-        } else {
-            // dfs and return if cycle 
-            dfs(neighbour, target);
-            if (cycled) return;
-        }
+      // add neighbour back since it is a cycle (round trip)
+      // we also reverse since we have going from last seen node -> beginning
+      // which is reverse of the order the question wants
+      path.push_back(neighbour);
+      std::reverse(path.begin(), path.end());
+      cycled = true;
+      return;
+    } else {
+      // dfs and return if cycle
+      dfs(neighbour, start);
+      if (cycled)
+        return;
     }
+  }
 }
 
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
 
-    int n, m;
-    std::cin >> n >> m;
+  int n, m;
+  std::cin >> n >> m;
 
-    UnionFind uf(n);
-    adj.resize(n + 1);
-    parent.resize(n + 1);
-    visited.resize(n + 1);
+  UnionFind uf(n);
+  adj.resize(n + 1);
+  parent.resize(n + 1);
+  visited.resize(n + 1);
 
-    for (int i = 0; i < m; i++) {
-        int s, e;
-        std::cin >> s >> e;
+  for (int i = 0; i < m; i++) {
+    int s, e;
+    std::cin >> s >> e;
 
-        adj[s].push_back(e);
-        adj[e].push_back(s);
+    adj[s].push_back(e);
+    adj[e].push_back(s);
 
-        if (!uf.union_sets(s, e)) {
-            // reset states
-            std::fill(parent.begin(), parent.end(), -1);
-            std::fill(visited.begin(), visited.end(), false);
-            path.clear();
-            cycled = false;
+    if (!uf.union_sets(s, e)) {
+      // reset states
+      std::fill(parent.begin(), parent.end(), -1);
+      std::fill(visited.begin(), visited.end(), false);
+      path.clear();
+      cycled = false;
 
-            // traverse with no parent
-            dfs(s, -1);
+      // traverse with no parent
+      dfs(s, -1);
 
-            // print out path if we encounter a cycle
-            // we check 4 according to problem statement
-            if (cycled && path.size() >= 4) {
-                std::cout << path.size() << std::endl;
-                for (int node : path) {
-                    std::cout << node << " ";
-                }
-                std::cout << std::endl;
-                return 0;
-            } else {
-                std::cout << "IMPOSSIBLE" << std::endl;
-                return 0;
-            }
+      // print out path if we encounter a cycle
+      // we check 4 according to problem statement
+      if (cycled && path.size() >= 4) {
+        std::cout << path.size() << std::endl;
+        for (int node : path) {
+          std::cout << node << " ";
         }
+        std::cout << std::endl;
+        return 0;
+      } else {
+        std::cout << "IMPOSSIBLE" << std::endl;
+        return 0;
+      }
     }
+  }
 
-    std::cout << "IMPOSSIBLE" << std::endl;
-    return 0;
+  std::cout << "IMPOSSIBLE" << std::endl;
+  return 0;
 }
